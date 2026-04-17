815f708d41110ade6c7b0a1aad078bbed5862fbe
julianNT <julian.nunez@sansano.usm.cl>
2026-04-17 18:46:28 -0400

    agrego Stage4 previamente borrado

---
+++ Stage4_V2/Stage4/Viewer.java
@@ -0,0 +1,20 @@
+
+import java.awt.geom.Point2D;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+public class Viewer {
+    public Viewer() {
+        
+    }
+
+
+
+
+
+    public void verFindMy(ETNube nube, String nombre){
+
+    System.out.print(nube.stringFormatoFigura2(nombre));
+
+    }
+}
